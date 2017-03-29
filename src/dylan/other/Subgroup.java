6a8da576e42f5be6d1a3b7070a5b0c79aa5e2ec2
Hotchocolate73 <dylan_mcneil@hotmail.co.uk>
2017-03-29 14:58:38 +0000

    Started subgroup implementation

---
+++ src/dylan/other/Subgroup.java
@@ -0,0 +1,17 @@
+package dylan.other;
+
+import dylan.units.Unit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subgroup {
+    List<Unit> units;
+    private int maxSize;
+
+    public Subgroup(){
+        units = new ArrayList<>();
+    }
+
+
+}
